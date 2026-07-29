d4443802bbcd6c45981a8c156bcc3d955cd3e685
Kratika Nigam <kratikanigam25@gmail.com>
2026-07-29 23:30:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        char str[]=s.toCharArray();
+        int n= str.length;
+        for(int i=0; i<n; i+=k*2){
+            int left=i;
+            int right= Math.min(left+k-1, n-1);
+
+            while(left <right){
+                char temp= str[left];
+                str[left]= str[right];
+                str[right]= temp;
+                left++;
+                right--;
+            }
+
+        }
+        return new String(str);
+    }
+}

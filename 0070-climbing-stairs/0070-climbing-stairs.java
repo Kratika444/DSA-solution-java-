c56a22181d3aa6aa228ee3f7880b85de62bceece
Kratika Nigam <kratikanigam25@gmail.com>
2026-08-08 20:44:02 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.47%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    static int[] dp;
+
+    public int climbStairs(int n) {
+        dp= new int[n+1];
+        Arrays.fill(dp, -1);
+        return climb(n);
+    }
+    public static int climb(int n ){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=climb(n-1)+ climb(n-2);
+    }
+}

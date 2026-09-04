018927978c2ee4ac9a69cdea6ad2d3b15587b208
Kratika Nigam <kratikanigam25@gmail.com>
2026-09-04 18:26:23 +0530

    Time: 2 ms (88.93%), Space: 43.7 MB (95.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,37 +1,62 @@
 class Solution {
     
-    public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0) return -1;
-        
-        for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid[0].length; j++) {
-                if(grid[i][j] == 2) rotAdjacent(grid, i, j, 2);
-            }
+    public class Triplet{
+        int row;
+        int col;
+        int time;
+        Triplet(int row, int col, int time){
+            this.row= row;
+            this.col=col;
+            this.time= time;
         }
-        
-        int minutes = 2;
-        for(int[] row : grid) {
-            for(int cell : row) {
-                if(cell == 1) return -1;
-                minutes = Math.max(minutes, cell);
-            }
-        }
-        
-        return minutes - 2;
     }
-    
-    private void rotAdjacent(int[][] grid, int i, int j, int minutes) {
-        if(i < 0 || i >= grid.length /* out of bounds */
-          || j < 0 || j >= grid[0].length /* out of bounds */
-          || grid[i][j] == 0 /* empty cell */
-          || (1 < grid[i][j] && grid[i][j] < minutes) /* this orange is already rotten by another rotten orange */
-          ) return;
-        else {
-            grid[i][j] = minutes;
-            rotAdjacent(grid, i - 1, j, minutes + 1);
-            rotAdjacent(grid, i + 1, j, minutes + 1);
-            rotAdjacent(grid, i, j - 1, minutes + 1);
-            rotAdjacent(grid, i, j + 1, minutes + 1);
+    public int orangesRotting(int[][] grid) {
+        int c=0;
+        int n = grid.length;
+        int m =grid[0].length;
+
+        Queue<Triplet> q= new LinkedList<>();
+        for(int i=0;i<n;i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {  // rotton
+                    q.add(new Triplet(i, j, 0));
+                }
+            }
         }
+            int maxtime=0;
+            while(q.size()>0){
+                Triplet front = q.remove();
+                int row= front.row;
+                int col= front.col;
+                int time= front.time;
+
+                maxtime= Math.max(maxtime, time);
+                // goint left -> row, col-1
+                if(col-1>=0 && grid[row][col-1]==1 ){
+                    grid[row][col-1]=2;
+                    q.add(new Triplet(row, col-1, time+1));
+                }
+                // going right -> row, col+1
+                if(col+1<m && grid[row][col+1]==1 ){
+                    grid[row][col+1]=2;
+                    q.add(new Triplet(row, col+1, time+1));
+                }
+                // going up -> row-1, col
+                if(row-1>=0 && grid[row-1][col]==1 ){
+                    grid[row-1][col]=2;
+                    q.add(new Triplet(row-1, col, time+1));
+                }
+                // going down -> row+1, col
+                if(row+1<n && grid[row+1][col]==1 ){
+                    grid[row+1][col]=2;
+                    q.add(new Triplet(row+1, col, time+1));
+                }
+            }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return maxtime;
     }
 }

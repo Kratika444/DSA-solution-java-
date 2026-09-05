830f184e94235d3c0f4f82e049f0bf03ca5c5476
Kratika Nigam <kratikanigam25@gmail.com>
2026-09-05 17:43:40 +0530

    Time: 8 ms (71.32%), Space: 54.2 MB (82.3%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,30 +1,73 @@
 class Solution {
-    public int numEnclaves(int[][] A) {
-        int result = 0;
-        for(int i = 0; i < A.length; i++) {
-            for(int j = 0; j < A[i].length; j++) {
-                if(i == 0 || j == 0 || i == A.length - 1 || j == A[i].length - 1)
-                    dfs(A, i, j);
-            }
+    public class Pair {
+        int row;
+        int col;
+
+        Pair(int row, int col){
+            this.row= row;
+            this.col=col;
         }
-        
-        for(int i = 0; i < A.length; i++) {
-            for(int j = 0; j < A[i].length; j++) {
-                if(A[i][j] == 1)
-                    result++;
-            }
-        }
-        
-        return result;
     }
-    
-    public void dfs(int a[][], int i, int j) {
-        if(i >= 0 && i <= a.length - 1 && j >= 0 && j <= a[i].length - 1 && a[i][j] == 1) {
-            a[i][j] = 0;
-            dfs(a, i + 1, j);
-            dfs(a, i - 1, j);
-            dfs(a, i, j + 1);
-            dfs(a, i, j - 1);
+
+    public int numEnclaves(int[][] grid) {
+       
+        int n = grid.length;
+        int m =grid[0].length;
+
+        Queue<Pair> q= new LinkedList<>();
+
+        for(int j=0;j<m;j++){
+            if(grid[0][j]==1){
+                grid[0][j]=-1;
+                q.add(new Pair(0,j));
+            }
+            if(grid[n-1][j]==1){
+                grid[n-1][j]=-1;
+                q.add(new Pair(n-1,j));
+            }
         }
+         for(int i=0;i<n;i++){
+            if(grid[i][0]==1){
+                grid[i][0]=-1;
+                q.add(new Pair(i,0));
+            }
+            if(grid[i][m-1]==1){
+                grid[i][m-1]=-1;
+                q.add(new Pair(i,m-1));
+            }
+        }
+            while(q.size()>0){
+                Pair front = q.remove();
+                int row= front.row;
+                int col= front.col;
+                
+                // going left -> row, col-1
+                if(col-1>=0 && grid[row][col-1]==1 ){
+                    grid[row][col-1]=-1;
+                    q.add(new Pair(row, col-1));
+                }
+                // going right -> row, col+1
+                if(col+1<m && grid[row][col+1]==1 ){
+                    grid[row][col+1]=-1;
+                    q.add(new Pair(row, col+1));
+                }
+                // going up -> row-1, col
+                if(row-1>=0 && grid[row-1][col]==1 ){
+                    grid[row-1][col]=-1;
+                    q.add(new Pair(row-1, col));
+                }
+                // going down -> row+1, col
+                if(row+1<n && grid[row+1][col]==1 ){
+                    grid[row+1][col]=-1;
+                    q.add(new Pair(row+1, col));
+                }
+            }
+            int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) count++;
+            }
+        }
+        return count;
     }
 }
